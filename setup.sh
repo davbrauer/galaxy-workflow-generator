b4b6016dc52253131008fd8664396a17d9d34517
koriege <koriege@gmail.com>
2018-10-10 19:45:41 +0200

    +bibtex citation, welcome page adaptions

--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@ sed -i -r 's/(^\s*)#+(\s*conda_auto_init\s*=).*/\1\2 True/' $GALAXY_CONFIG_FILE
 sed -i -r 's/(^\s*)#+(\s*conda_ensure_channels\s*=).*/\1\2 iuc,bioconda,conda-forge,defaults/' $GALAXY_CONFIG_FILE
 sed -i -r 's/(^\s*)#+(\s*new_user_dataset_access_role_default_private\s*=\s*).*/\1\2True/' $GALAXY_CONFIG_FILE
 sed -i -r 's/(^\s*)#+(\s*enable_quotas\s*=\s*).*/\1\2True/' $GALAXY_CONFIG_FILE
+sed -i -r 's/(^\s*)#+(\s*allow_user_dataset_purge\s*=\s*).*/\1\2True/' $GALAXY_CONFIG_FILE
 
 head -4 $GALAXY_ROOT/config/tool_conf.xml.sample > $GALAXY_ROOT/config/tool_conf.xml
 echo '  </section>' >> $GALAXY_ROOT/config/tool_conf.xml
@@ -23,9 +24,31 @@ tail -2 $GALAXY_ROOT/config/tool_conf.xml.sample >> $GALAXY_ROOT/config/tool_con
 
 sed -i -r 's/Get\s+Data/Upload Data/' $GALAXY_ROOT/integrated_tool_panel.xml
 
-mv /tmp/webhooks/switchtour $GALAXY_ROOT/config/plugins/webhooks
+mv /tmp/webhooks/* $GALAXY_ROOT/config/plugins/webhooks
 rm -rf /tmp/webhooks
 mv /tmp/guided_tours/*yaml $GALAXY_ROOT/config/plugins/tours/
 rm -rf /tmp/guided_tours
 cp -r /tmp/tools/* $GALAXY_ROOT/tools
 mv /tmp/tools/* $GALAXY_ROOT/test-data
+
+sed -i '$ d' $GALAXY_ROOT/config/tools/data_source/upload.xml
+cat << EOF >> $GALAXY_ROOT/config/tools/data_source/upload.xml
+  <citations>
+    <citation type="bibtex">@Article{de.STARI-workflow-generator,                           
+      author   = {Lott, Steffen C. and Wolfien, Markus and Riege, Konstantin and Bagnacani, Andrea and Wolkenhauer, Olaf and Hoffmann, Steve and Hess, Wolfgang R.},
+      title    = {Customized workflow development and data modularization concepts for {RNA}-{S}equencing and metatranscriptome experiments.},
+      journal  = {J. Biotechnol.},
+      year     = {2017},
+      volume   = {261},     
+      pages    = {85--96},      
+      month    = {Nov},   
+      issn     = {1873-4863},      
+      day      = {10},
+      doi      = {10.1016/j.jbiotec.2017.06.1203},       
+      language = {eng},
+      url      = {http://www.ncbi.nlm.nih.gov/pubmed/28676233},
+    }
+    </citation>
+  </citations>
+</tool>
+EOF
